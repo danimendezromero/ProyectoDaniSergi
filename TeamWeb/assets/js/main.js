d04cf1fd3f42df2b4bd95f4b12c8665a869dbb8e
Mofitex <32845654+Mofitex@users.noreply.github.com>
2018-11-30 17:46:59 +0100

    Add files via upload

--- TeamWeb/assets/js/main.js
+++ TeamWeb/assets/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
+    var elems2 = document.querySelectorAll('.collapsible');
+    var instances2 = M.Collapsible.init(elems, options);
 });
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
@@ -8,5 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
 // Or with jQuery
 
 $(document).ready(function(){
+  $('.collapsible').collapsible();
   $('.sidenav').sidenav();
+  $(".dropdown-trigger").dropdown({
+    hover: true
+  });
 });
