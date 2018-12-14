ca5b11805eb34c22f808bd92dbeead2ad9bd4bb6
Mofitex <32845654+Mofitex@users.noreply.github.com>
2018-12-14 18:15:14 +0100

    Add files via upload

--- TeamWeb/assets/js/main.js
+++ TeamWeb/assets/js/main.js
@@ -1,19 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-    var elems2 = document.querySelectorAll('.collapsible');
-    var instances2 = M.Collapsible.init(elems, options);
-});
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-// Or with jQuery
+
 
 $(document).ready(function(){
+  var slider = document.getElementById('test-slider');
+    noUiSlider.create(slider, {
+     start: [18, 25],
+     connect: true,
+     step: 1,
+     orientation: 'horizontal', // 'horizontal' or 'vertical'
+     range: {
+       'min': 0,
+       'max': 100     },
+     format: wNumb({
+       decimals: 0
+     })
+    });
+  $('.datepicker').datepicker();
+  $('select').formSelect();
   $('.collapsible').collapsible();
   $('.sidenav').sidenav();
   $(".dropdown-trigger").dropdown({
-    hover: true
+    hover: false
   });
-   $('.tabs').tabs();
+
+
+
 });
