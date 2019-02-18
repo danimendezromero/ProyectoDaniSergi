d2ed3ad88fed5a89ac6217574d30198a38c68fab
Mofitex <32845654+Mofitex@users.noreply.github.com>
2019-02-18 19:53:22 +0100

    Add files via upload

---
+++ TeamWeb/buscar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- ========== Favicon ============== -->
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon_template.png"/>
+    <!-- ========== Fonts ============== -->
+    <link rel="stylesheet" href="" >
+    <!-- ========== CSS ============== -->
+    <link rel="stylesheet" href="assets/css/materialize.min.css">
+    <link rel="stylesheet" href="assets/css/estils.css">
+    <!-- ========== Icons ============== -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- ========== JavaScript ============== -->
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/materialize.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script type="text/javascript" src="assets/js/buscar.js"></script>
+    <title>Home</title>
+  </head>
+  <body>
+    <?php include('assets/php/navbar.php') ?>
+    <!-- ========== Start of Content ============== -->
+    <div id="asdf" class="container">
+
+    </div>
+
+    <!-- ========== End of Content ============== -->
+    <?php include('assets/php/footer.php') ?>
+  </body>
+</html>
